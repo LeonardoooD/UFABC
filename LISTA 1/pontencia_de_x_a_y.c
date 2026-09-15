79bd569f7289bd13a056524d3bb7393464737fd4
Leonardo Dias <112840888+LeonardoooD@users.noreply.github.com>
2026-09-15 18:39:37 -0300

    Delete LISTA 1 directory

--- LISTA 1/pontencia_de_x_a_y.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int x, y, pot  ;
-    scanf("%d %d", &x, &y);
-    for(pot = 1 ; y > 0; pot = pot * x, y--){
-    }
-    printf("%d", pot);
-}
--- LISTA 1/soma_de_gauss.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int valor, resultado;
-    scanf("%d", &valor );
-    resultado = (valor * ( 1 + valor )) / 2;
-    printf("soma de 1 a %d = %d", valor , resultado);
-    return 0;
-}
